efcec89bcbda1b4b529870a503916f8b54153b3f
hmoa01 <hmoa01@gmail.com>
2024-06-13 06:36:11 +0200

    add error handling and validation

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -6,20 +6,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import AuthVerificationTokenModel from "src/models/authVerificationToken";
+import { sendErrorRes } from "src/utils/helper";
 
 export const createNewUser: RequestHandler = async (req, res) => {
   const { email, password, name } = req.body;
 
-  if (!name) return res.status(422).json({ message: "Name is missing" });
-  if (!email) return res.status(422).json({ message: "Email is missing" });
-  if (!password)
-    return res.status(422).json({ message: "Password is missing" });
+  // if (!name) return sendErrorRes(res, "Name is missing", 422);
+  // if (!email) return sendErrorRes(res, "Email is missing", 422);
+  // if (!password) return sendErrorRes(res, "Password is missing", 422);
 
   const existingUser = await UserModel.findOne({ email });
   if (existingUser)
-    return res
-      .status(401)
-      .json({ message: "Unauthorized request, email is already in use!" });
+    return sendErrorRes(
+      res,
+      "Unauthorized request, email is already in use!",
+      401
+    );
 
   const user = await UserModel.create({ email, password, name });
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
+import "express-async-errors";
 import "src/db";
-import express, { RequestHandler } from "express";
+import express from "express";
 import authRouter from "routes/auth";
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,5 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 
 //API ROUTES
 app.use("/auth", authRouter);
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({ message: err.message });
+} as express.ErrorRequestHandler);
 
 app.listen(8000, () => console.log("The app running on http://localhost:8000"));
