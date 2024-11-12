03d0c1676d9107e8484a559a67932d9d10962d60
hmoa01 <hmoa01@gmail.com>
2024-11-12 17:41:54 +0100

    add remove image

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import authRouter from "routes/auth";
 import productRouter from "./routes/product";
 import "dotenv/config";
+import { sendErrorRes } from "./utils/helper";
 
 const app = express();
 
@@ -20,5 +21,9 @@ app.use("/product", productRouter);
 app.use(function (err, req, res, next) {
   res.status(500).json({ message: err.message });
 } as express.ErrorRequestHandler);
+
+app.use("*", (req, res) => {
+  sendErrorRes(res, "Not Found!", 404);
+});
 
 app.listen(8000, () => console.log("The app running on http://localhost:8000"));
