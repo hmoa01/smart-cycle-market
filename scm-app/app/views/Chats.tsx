29411bbaefa3af945e83388cf535c0335ee187b9
hmoa01 <hmoa01@gmail.com>
2024-12-02 08:24:18 +0100

    conversation routes and socket

--- scm-app/app/views/Chats.tsx
+++ scm-app/app/views/Chats.tsx
@@ -1,11 +1,45 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { View, StyleSheet, Text, Platform, StatusBar } from "react-native";
 import AppHeader from "../components/AppHeader";
 import BackButton from "../ui/BackButton";
+import EmptyView from "../ui/EmptyView";
+import useClient from "../hooks/useClient";
+import { runAxiosAsync } from "../api/runAxiosAsync";
 
 interface Props {}
 
 const Chats: FC<Props> = (props) => {
+  const { authClient } = useClient();
+  const chats = [];
+
+  const fetchLastChats = async () => {
+    const res = await runAxiosAsync<{
+      chats: {
+        id: string;
+        lastMessage: string;
+        timestamp: Date;
+        unreadChatCounts: number;
+        peerProfile: { id: string; name: string; avatar?: string };
+      };
+    }>(authClient("/conversation/last-chats"));
+
+    if (res) {
+      console.log(res.chats);
+    }
+  };
+
+  useEffect(() => {
+    fetchLastChats();
+  }, []);
+
+  if (!chats.length)
+    return (
+      <>
+        <AppHeader style={{ marginTop: 20 }} backButton={<BackButton />} />
+        <EmptyView title="There is no chats." />
+      </>
+    );
+
   return (
     <View style={styles.container}>
       <AppHeader backButton={<BackButton />} />
