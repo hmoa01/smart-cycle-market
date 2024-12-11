edc12d01f8de73032c1be60a559fa121a3184f2f
hmoa01 <hmoa01@gmail.com>
2024-12-11 21:27:17 +0100

    add mail send with real mail

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -1,4 +1,9 @@
 import nodemailer from "nodemailer";
+import { MailtrapClient } from "mailtrap";
+
+const TOKEN = process.env.NODEMAILER_TOKEN!;
+
+const client = new MailtrapClient({ token: TOKEN });
 
 const transport = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
@@ -9,12 +14,43 @@ const transport = nodemailer.createTransport({
   },
 });
 
-const sendVerification = async (email: string, link: string) => {
+const sendTestVerificationMail = async (email: string, link: string) => {
   await transport.sendMail({
     from: process.env.NODEMAILER_EMAIL,
     to: email,
     html: `<h1>Please click on <a href="${link}"> this link</a> to verify your account.</h1>`,
   });
+};
+
+const sendVerificationMailProd = async (email: string, link: string) => {
+  const sender = {
+    email: "support@ahmedlemes.com",
+    name: "Smart Cycle Market",
+  };
+
+  const recipients = [
+    {
+      email,
+    },
+  ];
+
+  client
+    .send({
+      from: sender,
+      to: recipients,
+      template_uuid: "d1e740c4-db04-4669-95df-943a6ec7de67",
+      template_variables: {
+        confirmation_link: link,
+        user_name: email,
+      },
+    })
+    .then(console.log, console.error);
+};
+
+const sendVerification = async (email: string, link: string) => {
+  if (process.env.NODE_ENV === "development")
+    sendTestVerificationMail(email, link);
+  else sendVerificationMailProd(email, link);
 };
 
 const sendPasswordResetLink = async (email: string, link: string) => {
